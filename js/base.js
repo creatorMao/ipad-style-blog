08930f6d67df9aaed2895806b903492ec006c34e
Zhiwu Mao <zwmao@outlook.com>
2019-10-27 19:46:06 +0800

    细微调整
    - 打开app时清除未读消息
    - 增加微博
    - 日期显示

--- js/base.js
+++ js/base.js
@@ -13,8 +13,30 @@
  */
 (function () {
     var dateHelper = {
-        getDate:function() {
-           
+        //获取星期几
+        getDay:function(isConvertToText) {
+           var dt=new Date();
+           var day=dt.getDay();
+           if(isConvertToText)
+           {
+                var weekDay=["星期日","星期一","星期二","星期三","星期四","星期五","星期六"];
+                return weekDay[day];
+           }
+           return day;
+        },
+        //获取时间 几点几分
+        getTime:function (split) {
+            var dt=new Date();
+           if(!split)
+           {
+                split=":";
+           }
+           return dt.getHours()+split+dt.getMinutes();
+        },
+        //获取某月某日
+        getDate:function(){
+            var dt=new Date();
+            return (dt.getMonth()+1)+"月"+(dt.getDate())+"日";
         }
     };
     window.commonTools.dateHelper = dateHelper;
